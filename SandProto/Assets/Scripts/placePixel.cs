f91a27cb77bea72c4c991e5ac0062510601df9f9
JB <jackbruce101@gmail.com>
2020-02-01 14:16:13 +0800

    Placing pixels

---
+++ SandProto/Assets/Scripts/placePixel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class placePixel : MonoBehaviour
+{
+
+    enum pixelPallete
+    {
+        sand,
+        rock,
+        water
+    }
+
+    public GameObject pixel;
+    pixelPallete selectedPixel = pixelPallete.sand;
+
+    void spawnPixel(pixelPallete inPixel)
+    {
+        Camera camera = GetComponent<Camera>();
+        Vector3 placementCoordinate = Camera.main.ScreenToWorldPoint(Input.mousePosition); //(position translated from screen to world point)
+        placementCoordinate.z = 0; // Set z at 0 so camera can see it
+        GameObject pixelObj = Object.Instantiate(pixel, placementCoordinate, new Quaternion()); // Spawn pixel, at mouse position, with 0 rotation.
+        //pixelObj.pixelType = inPixel; // Tell the pixel what type it is.
+    }
+
+    void Update()
+    {
+        // Place pixel:
+        if (Input.GetMouseButtonDown(0))
+            spawnPixel(selectedPixel);
+    }
+}
